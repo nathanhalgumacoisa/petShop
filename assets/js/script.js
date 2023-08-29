6afb087255be57fcaf1b56291ec8f9ea02d4c1e5
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-08-29 10:45:39 -0300

    new feat: criarIdade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 //começando com a class Pet
 class Pet{
-    constructor(tutor, nome, especie, foto, data){
+    constructor(tutor, nome, especie, foto, nascimento){
         this.tutor = tutor;
         this.nome = nome;
         this.especie = especie;
         this.foto = foto;
-        this.data = data;
+        this.nascimento = nascimento;
+    }
+    criarIdade(){
+        let hoje = new Date();
+        let nascimento = new Date(this.nascimento);
+        let idade = hoje.getFullYear() - nascimento.getFullYear();
+        let mes = hoje.getMonth() - nascimento.getMonth();
+        if(mes < 0 || (mes === 0 && hoje.getDate()< nascimento.getDate())){
+            idade--;
+        }
+        return idade;
     }
 }
 //criando a class ListaPet  para criar o parametro do array
@@ -24,19 +34,21 @@ function criarPet(){
     const nome = document.getElementById("nome").value;
     const especie = document.getElementById("especie").value;
     const foto = document.getElementById("foto").value;
-    const data = document.getElementById("data").value;
+    const nascimento = document.getElementById("data").value;
 //criando  variavel pet para colocar as informações dos inputs pro objeto
-    const pet = new Pet(tutor, nome, especie, foto, data);
+    const pet = new Pet(tutor, nome, especie, foto, nascimento);
     petsarray.adicionar(pet);
     console.log(petsarray);
     //chamando as functions
-    limparCampos()
+    limparCampos();
 }
-//criação da função limparCampos para... limpar os campos
+//criação da function limparCampos para... limpar os campos
 function limparCampos(){
-     document.getElementById("tutor").value = ""
-     document.getElementById("nome").value = ""
-     document.getElementById("especie").value = ""
-     document.getElementById("foto").value = ""
-     document.getElementById("data").value = ""
+     document.getElementById("tutor").value = "";
+     document.getElementById("nome").value = "";
+     document.getElementById("especie").value = "";
+     document.getElementById("foto").value = "";
+     document.getElementById("data").value = "";
 }
+
+
